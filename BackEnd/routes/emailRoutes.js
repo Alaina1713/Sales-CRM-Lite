24d0e2783aa5da0a0907d7060c4b8e71fcb7f9a6
Alaina Rahim <156155377+Alaina1713@users.noreply.github.com>
2025-09-26 20:54:11 +0530

    Delete BackEnd directory

--- BackEnd/routes/emailRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { protect } from "../middleware/authMiddleware.js";
-import { sendEmailController, getEmailsController } from "../controllers/emailController.js";
-
-const router = express.Router();
-
-router.route("/").get(protect, getEmailsController).post(protect, sendEmailController);
-
-export default router;
